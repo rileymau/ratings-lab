1b57c82a5006603f75530db8095a5490507462a6
Molly Riley <mo.riley@gmail.com>
2021-10-04 20:57:00 +0000

    edits file 2

--- ratings2.py
+++ ratings2.py
@@ -1,21 +1,41 @@
 """Restaurant rating lister."""
 import sys
 input_file = open(sys.argv[1])
-rating_dict = {}
 
-for line in input_file:
-    name, rating = line.rstrip().split(":")
-    rating_dict[name] = rating
+while True:
+    print("what would you like to do ?")
+    print("Seeing all the ratings in alphabetical order?")
+    print("Adding a new restaurant?")
+    print("Quitting?")
+    print("Please enter S, A, or Q")
+    response = input()
+    
+    if response == 'Q':
+        break
+    elif response == "S":
+        print_rating()
+    elif response == "A":
+        restaurant_name = input("What is the restraunt name? ")
+        restaurant_rating = input("What is their rating? Choose an integer between 1 and 5. ")
+        if 5 < int(restaurant_rating) < 1:
+           input("Please re-enter the rating !\n")
+        else:
+            def add_to_dict():
+                print_rating()
+                rating_dict[restaurant_name] = int(restaurant_rating)
 
-input_file.close()
 
-restaurant_name = input("What is the restraunt name? ")
-restaurant_rating = input("What is their rating?")
-rating_dict[restaurant_name] = int(restaurant_rating)
-
-sorted_dict = sorted(rating_dict.keys())
-for key in sorted_dict: 
-      print(f"{key} is rated at {rating_dict[key]}")
+def print_rating():
+    rating_dict = {}
+    for line in input_file:
+        name, rating = line.rstrip().split(":")
+        rating_dict[name] = rating
+    input_file.close()
+    
+    sorted_dict = sorted(rating_dict.keys())
+    for key in sorted_dict: 
+        print(f"{key} is rated at {rating_dict[key]}")
+    return
 
 
 
